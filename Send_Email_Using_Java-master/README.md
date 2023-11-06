db81a7fd6823dcdb0845158bc9e6a3b64ab31b00
Gulamnabi <85071613+Gulamnabi7786@users.noreply.github.com>
2023-11-06 17:42:30 +0900

    Add files via upload

---
+++ Send_Email_Using_Java-master/README.md
@@ -0,0 +1,10 @@
+# About Send Email Project
+
+* How to send **Email** message using **Java**.
+* This program written in java.
+* Using the configurations of **gmail SMTP**.
+* This program needs **External Library's**
+* Don't worry you can find them inside the program.
+* If you want to download them [here](https://github.com/AhmedAbuwarda/SendEmail/tree/master/src/Lib).
+
+* **Finally, I hope you enjoy it**.
---
+++ Send_Email_Using_Java-master/src/JavaMail/SendEmailWithJava.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template file, choose Settings | Editor | File and Code Templates
+ * and change the template in the editor.
+ */
+
+package JavaMail;
+
+import com.sun.mail.smtp.SMTPTransport;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmailWithJava {
+
+    // Data Fields.
+    private static final String EMAIL_TO = "reciver@gmail.com";
+    private static final String EMAIL_SUBJECT = "Email testing java program";
+    private static final String EMAIL_TEXT = "Email recieved sucessfully !!";
+    private static final String EMAIL_FROM = "sender@gmail.com";
+    private static final String EMAIL_PASSWORD = "password";
+
+    /**
+     * @param args the command line arguments.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here.
+
+        System.out.println("Loading Data...");
+
+        // Create New Properties.
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com"); //optional, defined in SMTPTransport
+        properties.put("mail.smtp.port", "587"); // default port 587
+
+        // Create New Session.
+        Session session = Session.getInstance(properties, null);
+        Message message = new MimeMessage(session);
+
+        // Try Catch Block.
+        try {
+
+            // The Message.
+            message.setFrom(new InternetAddress(EMAIL_FROM));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(EMAIL_TO, false));
+            message.setSubject(EMAIL_SUBJECT);
+            message.setText(EMAIL_TEXT);
+
+            // The Connection.
+            System.out.println("Connecting...");
+            SMTPTransport transport = (SMTPTransport) session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", EMAIL_FROM, EMAIL_PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
+            System.out.println("Response: " + transport.getLastServerResponse() + "Message Sent!");
+            transport.close();
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
